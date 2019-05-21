ef80cf8d320467e01b800ce6e0f83cf6f6c03bca
Michel VELMON <michel.velmon@icloud.com>
2019-05-21 20:26:21 +0200

    2nd : sevral updates at url.py and settings.py files + new files gandi.yml, fig.yml

--- project/project/urls.py
+++ project/project/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', include('nakr_consulting.urls', namespace = 'nakr_consulting')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^', include('nakr_consulting.urls', namespace = 'nakr_consulting')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
